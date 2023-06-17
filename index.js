861671ced1b553fcdb46088de755517b4a7afd57
Melvin Jones Repol <mrepol742@gmail.com>
2023-06-17 19:39:02 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,111 +1,181 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
+var __awaiter =
+    (this && this.__awaiter) ||
+    function (thisArg, _arguments, P, generator) {
+        function adopt(value) {
+            return value instanceof P
+                ? value
+                : new P(function (resolve) {
+                      resolve(value);
+                  });
+        }
+        return new (P || (P = Promise))(function (resolve, reject) {
+            function fulfilled(value) {
+                try {
+                    step(generator.next(value));
+                } catch (e) {
+                    reject(e);
+                }
             }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
+            function rejected(value) {
+                try {
+                    step(generator["throw"](value));
+                } catch (e) {
+                    reject(e);
+                }
+            }
+            function step(result) {
+                result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+            }
+            step((generator = generator.apply(thisArg, _arguments || [])).next());
+        });
+    };
+var __generator =
+    (this && this.__generator) ||
+    function (thisArg, body) {
+        var _ = {
+                label: 0,
+                sent: function () {
+                    if (t[0] & 1) throw t[1];
+                    return t[1];
+                },
+                trys: [],
+                ops: [],
+            },
+            f,
+            y,
+            t,
+            g;
+        return (
+            (g = { next: verb(0), throw: verb(1), return: verb(2) }),
+            typeof Symbol === "function" &&
+                (g[Symbol.iterator] = function () {
+                    return this;
+                }),
+            g
+        );
+        function verb(n) {
+            return function (v) {
+                return step([n, v]);
+            };
+        }
+        function step(op) {
+            if (f) throw new TypeError("Generator is already executing.");
+            while ((g && ((g = 0), op[0] && (_ = 0)), _))
+                try {
+                    if (((f = 1), y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done)) return t;
+                    if (((y = 0), t)) op = [op[0] & 2, t.value];
+                    switch (op[0]) {
+                        case 0:
+                        case 1:
+                            t = op;
+                            break;
+                        case 4:
+                            _.label++;
+                            return { value: op[1], done: false };
+                        case 5:
+                            _.label++;
+                            y = op[1];
+                            op = [0];
+                            continue;
+                        case 7:
+                            op = _.ops.pop();
+                            _.trys.pop();
+                            continue;
+                        default:
+                            if (!((t = _.trys), (t = t.length > 0 && t[t.length - 1])) && (op[0] === 6 || op[0] === 2)) {
+                                _ = 0;
+                                continue;
+                            }
+                            if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) {
+                                _.label = op[1];
+                                break;
+                            }
+                            if (op[0] === 6 && _.label < t[1]) {
+                                _.label = t[1];
+                                t = op;
+                                break;
+                            }
+                            if (t && _.label < t[2]) {
+                                _.label = t[2];
+                                _.ops.push(op);
+                                break;
+                            }
+                            if (t[2]) _.ops.pop();
+                            _.trys.pop();
+                            continue;
+                    }
+                    op = body.call(thisArg, _);
+                } catch (e) {
+                    op = [6, e];
+                    y = 0;
+                } finally {
+                    f = t = 0;
+                }
+            if (op[0] & 5) throw op[1];
+            return { value: op[0] ? op[1] : void 0, done: true };
+        }
+    };
 Object.defineProperty(exports, "__esModule", { value: true });
-var axios = require("axios");
-var tf = require("@tensorflow/tfjs-node");
-var nsfw = require("nsfwjs");
+//import * as tf from '@tensorflow/tfjs-node';
+//import * as nsfw from 'nsfwjs';
 var http = require("http");
 var model;
-var loadModel = function () { return __awaiter(void 0, void 0, void 0, function () {
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, nsfw.load()];
-            case 1:
-                model = _a.sent();
-                return [2 /*return*/];
-        }
+var loadModel = function () {
+    return __awaiter(void 0, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            return [2 /*return*/];
+        });
     });
-}); };
+};
 var server = http.createServer(getRoutes());
-loadModel().then(function () { return server.listen(7421, function () {
+/*
+loadModel().then(() => server.listen(7421, function () {
+  console.log("server_status", "7421")
+}))
+*/
+server.listen(7421, function () {
     console.log("server_status", "7421");
-}); });
+});
 function getRoutes() {
     return function (req, res) {
         return __awaiter(this, void 0, void 0, function () {
-            var ress, url, results;
+            var ress, url;
             return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0:
-                        ress = req.url;
-                        url = ress.split("?")[0].replace("url=", "");
-                        console.log(req.method, req.headers.origin, url);
-                        return [4 /*yield*/, main(url)];
-                    case 1:
-                        results = _a.sent();
-                        res.setHeader("Content-Type", "text/json");
-                        res.writeHead(200);
-                        res.end(results);
-                        return [2 /*return*/];
+                ress = req.url;
+                if (ress.includes("?url=")) {
+                    url = ress.split("?url=")[1];
+                    console.log(req.method, req.headers.origin, ress);
+                    //  let results = await main(url);
+                    res.setHeader("Content-Type", "text/json");
+                    res.writeHead(200);
+                    res.end(url);
+                } else {
+                    res.setHeader("Content-Type", "text/json");
+                    res.writeHead(200);
+                    res.end('{result: "error invalid parameter"}');
                 }
+                return [2 /*return*/];
             });
         });
     };
 }
-function main(url) {
-    return __awaiter(this, void 0, void 0, function () {
-        var pic, image, predictions;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4 /*yield*/, axios.get(url, {
-                        responseType: 'arraybuffer',
-                    })];
-                case 1:
-                    pic = _a.sent();
-                    return [4 /*yield*/, tf.node.decodeImage(pic.data)];
-                case 2:
-                    image = _a.sent();
-                    return [4 /*yield*/, model.classify(image)];
-                case 3:
-                    predictions = _a.sent();
-                    image.dispose();
-                    if (Math.floor((predictions[0].probability / 1.0) * 100) > 90) {
-                        return [2 /*return*/, '{result: "Drawing"}'];
-                    }
-                    else if (Math.floor((predictions[2].probability / 1.0) * 100) > 90 || Math.floor((predictions[3].probability / 1.0) * 100) > 90
-                        || Math.floor((predictions[4].probability / 1.0) * 100) > 90) {
-                        return [2 /*return*/, '{result: "Explicit"}'];
-                    }
-                    else {
-                        return [2 /*return*/, '{result: "Nothing"}'];
-                    }
-                    return [2 /*return*/];
-            }
-        });
-    });
+/*
+async function main(url) {
+  const pic = await axios.get(url, {
+    responseType: 'arraybuffer',
+  })
+
+  const image = await tf.node.decodeImage(pic.data)
+  const predictions = await model.classify(image)
+  image.dispose()
+  if (Math.floor((predictions[0].probability / 1.0) * 100) > 90) {
+    return '{result: "Drawing"}';
+  } else if (Math.floor((predictions[2].probability / 1.0) * 100) > 90 || Math.floor((predictions[3].probability / 1.0) * 100) > 90
+     || Math.floor((predictions[4].probability / 1.0) * 100) > 90) {
+    return '{result: "Explicit"}';
+  } else {
+    return '{result: "Nothing"}';
+  }
 }
+*/
--- index.ts
+++ index.ts
@@ -1,46 +1,57 @@
-import * as axios from 'axios';
-import * as tf from '@tensorflow/tfjs-node';
-import * as nsfw from 'nsfwjs';
-import * as http from 'http';
+import * as axios from "axios";
+import * as tf from "@tensorflow/tfjs-node";
+import * as nsfw from "nsfwjs";
+import * as http from "http";
 
 let model;
 
 const loadModel = async () => {
-  model = await nsfw.load()
-}
+    model = await nsfw.load();
+};
 
 const server = http.createServer(getRoutes());
 
-loadModel().then(() => server.listen(7421, function () {
-  console.log("server_status", "7421")
-}))
+loadModel().then(() =>
+    server.listen(7421, function () {
+        console.log("server_status", "7421");
+    })
+);
+
+server.listen(7421, function () {
+    console.log("server_status", "7421");
+});
 
 function getRoutes() {
-  return async function (req, res) {
-      let ress = req.url;
-      let url = ress.split("?")[0].replace("url=", "");
-      console.log(req.method, req.headers.origin, url);
-      let results = await main(url);
-      res.setHeader("Content-Type", "text/json");
-      res.writeHead(200);
-      res.end(results);
-  }
+    return async function (req, res) {
+        let ress = req.url;
+        if (ress.includes("?url=")) {
+            let url = ress.split("?url=")[1];
+            console.log(req.method, req.headers.origin, ress);
+            let results = await main(url);
+            res.setHeader("Content-Type", "text/json");
+            res.writeHead(200);
+            res.end(url);
+        } else {
+            res.setHeader("Content-Type", "text/json");
+            res.writeHead(200);
+            res.end('{result: "error invalid parameter"}');
+        }
+    };
 }
 
 async function main(url) {
-  const pic = await axios.get(url, {
-    responseType: 'arraybuffer',
-  })
+    const pic = await axios.get(url, {
+        responseType: "arraybuffer",
+    });
 
-  const image = await tf.node.decodeImage(pic.data)
-  const predictions = await model.classify(image)
-  image.dispose()
-  if (Math.floor((predictions[0].probability / 1.0) * 100) > 90) {
-    return '{result: "Drawing"}';
-  } else if (Math.floor((predictions[2].probability / 1.0) * 100) > 90 || Math.floor((predictions[3].probability / 1.0) * 100) > 90
-     || Math.floor((predictions[4].probability / 1.0) * 100) > 90) {
-    return '{result: "Explicit"}';
-  } else {
-    return '{result: "Nothing"}';
-  }
+    const image = await tf.node.decodeImage(pic.data);
+    const predictions = await model.classify(image);
+    image.dispose();
+    if (Math.floor((predictions[0].probability / 1.0) * 100) > 90) {
+        return '{result: "Drawing"}';
+    } else if (Math.floor((predictions[2].probability / 1.0) * 100) > 90 || Math.floor((predictions[3].probability / 1.0) * 100) > 90 || Math.floor((predictions[4].probability / 1.0) * 100) > 90) {
+        return '{result: "Explicit"}';
+    } else {
+        return '{result: "Nothing"}';
+    }
 }
